49774d7a17f3b85f390d7a0542cd5c2291118eba
Luke Lai <lukelai.dev@gmail.com>
2022-08-22 15:43:19 +0100

    Fetch location API data to set city and country

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 
 import sunIcon from "../assets/desktop/icon-sun.svg";
+import moonIcon from "../assets/desktop/icon-moon.svg";
 import downArrow from "../assets/desktop/icon-arrow-down.svg";
 
 import worldTime from "../models/worldtime";
@@ -8,14 +9,21 @@ import worldTime from "../models/worldtime";
 const Info = () => {
   const [timeData, setTimeData] = useState<worldTime>();
   const [time, setTime] = useState("");
+  const [greeting, setGreeting] = useState("");
+  const [ip, setIp] = useState("");
+  const [location, setLocation] = useState<{ city: string; country: string }>();
 
+  //Fetch Time from API
   const fetchLocalTimeHandler = async () => {
     const response = await fetch("http://worldtimeapi.org/api/ip");
     const data = await response.json();
 
-    const { datetime } = data;
+    const { datetime, client_ip } = data;
     setTimeData(data);
 
+    setIp(client_ip);
+
+    //Clock time
     const date = new Date(datetime);
     setTime(
       date.toLocaleTimeString("en-US", {
@@ -24,33 +32,74 @@ const Info = () => {
         hour12: false,
       })
     );
+
+    //Greetings
+
+    const hour = Number(
+      date.toLocaleTimeString("en-US", { hour: "2-digit", hour12: false })
+    );
+
+    if (hour >= 5 && hour < 12) {
+      setGreeting("Morning");
+    } else if (hour >= 12 && hour < 18) {
+      setGreeting("Afternoon");
+    } else {
+      setGreeting("Evening");
+    }
   };
 
-  //   const convertTime = useCallback(() => {
-  //     const date = new Date(timeData!.datetime);
-  //     setTime(
-  //       date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-  //     );
-  //   }, [timeData]);
+  //Fetch location from API
+
+  const fetchLocation = async () => {
+    const response = await fetch(
+      `https://api.ipbase.com/v2/info?apikey=mxlNYo3o6QzD0eVEzurJlMJLFRw2ExCfqqZ0VKCi&language=en&ip=${ip}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+
+    const city = data.data.location.city.name;
+    const country = data.data.location.country.name;
+
+    console.log(city, country);
+    setLocation({ city, country });
+  };
 
   useEffect(() => {
     fetchLocalTimeHandler();
+    fetchLocation();
+
+    const interval = setInterval(() => {
+      fetchLocalTimeHandler();
+    }, 60000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <section className="info-container">
       <div className="info">
         <div className="info__heading">
-          <img className="info__heading__icon" src={sunIcon} alt="Sun Icon" />
-          <h4 className="info__heading__h4">GOOD MORNING, IT’S CURRENTLY</h4>
+          {greeting === "Evening" ? (
+            <img
+              className="info__heading__icon"
+              src={moonIcon}
+              alt="Moon Icon"
+            />
+          ) : (
+            <img className="info__heading__icon" src={sunIcon} alt="Sun Icon" />
+          )}
+          <h4 className="info__heading__h4">GOOD {greeting}, IT’S CURRENTLY</h4>
         </div>
         <div className="info__clock">
           <h1>
             <span className="info__clock__time">{time}</span>
-            <span className="info__clock__zone">BST</span>
+            <span className="info__clock__zone">{timeData?.abbreviation}</span>
           </h1>
         </div>
-        <h3 className="info__location">IN LONDON, UK</h3>
+        <h3 className="info__location">
+          IN {location?.city}, {location?.country}
+        </h3>
       </div>
       <button className="toggle">
         <span className="toggle--text">More</span>

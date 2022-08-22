57c98d2f477274b0adce7be0f25a6bd042b09e83
Luke Lai <lukelai.dev@gmail.com>
2022-08-22 14:16:34 +0100

    Fetch quotes API data

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,14 +1,40 @@
+import { useState, useEffect, useCallback } from "react";
 import refreshIcon from "../assets/desktop/icon-refresh.svg";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 const Quote = () => {
+  const [quote, setQuote] = useState<{
+    id: string;
+    author: string;
+    en: string;
+  }>({ id: "", author: "", en: "" });
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  const fetchQuoteHandler = useCallback(async () => {
+    setIsAnimated(false);
+    const response = await fetch(
+      "https://programming-quotes-api.herokuapp.com/Quotes/random"
+    );
+    const data = await response.json();
+
+    setQuote(data);
+    setIsAnimated(true);
+  }, []);
+
+  useEffect(() => {
+    fetchQuoteHandler();
+  }, [fetchQuoteHandler]);
+
   return (
     <div className="quote-container">
-      <p className="quote-container__quote">
-        “The science of operations, as derived from mathematics more especially,
-        is a science of itself, and has its own abstract truth and value.”
-      </p>
-      <p className="quote-container__author">Ada Lovelace</p>
-      <button className="quote-container__refresh">
+      <CSSTransition in={isAnimated} timeout={2000} classNames="fade">
+        <div>
+          <p className="quote-container__quote">{quote.en}</p>
+          <p className="quote-container__author">{quote.author}</p>
+        </div>
+      </CSSTransition>
+
+      <button onClick={fetchQuoteHandler} className="quote-container__refresh">
         <img src={refreshIcon} alt="Refresh Icon" />
       </button>
     </div>
---
+++ src/models/quote.ts
@@ -0,0 +1,13 @@
+class NewQuote {
+  id: string;
+  author: string;
+  en: string;
+
+  constructor(id: string, author: string, en: string) {
+    this.id = id;
+    this.author = author;
+    this.en = en;
+  }
+}
+
+export default NewQuote;

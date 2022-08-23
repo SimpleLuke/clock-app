437961318e18c8f7ebe6355ccfed6f4ead633ff5
Luke Lai <lukelai.dev@gmail.com>
2022-08-23 13:08:12 +0100

    Turn off the location fetching

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -76,7 +76,7 @@ const Info = () => {
 
   useEffect(() => {
     fetchLocalTimeHandler();
-    fetchLocation();
+    // fetchLocation();
 
     const interval = setInterval(() => {
       fetchLocalTimeHandler();
